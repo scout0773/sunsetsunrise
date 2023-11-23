1081b17cd174bf171e8c7c0e9c1c115d7b2ff209
matthew atkins <scout0773@matthews-Air.lan>
2023-11-23 00:04:52 -0500

    prettier format

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,68 +5,80 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function fetchCoordinatesAndData() {
-    const addressInput = document.getElementById('locationSearch');
-    const address = addressInput.value.trim();
+  const addressInput = document.getElementById("locationSearch");
+  const address = addressInput.value.trim();
 
-    if (!address) {
-        alert('Please enter a location');
-        return;
-    }
+  if (!address) {
+    alert("Please enter a location");
+    return;
+  }
 
-    addressInput.value = '';
+  addressInput.value = "";
 
-    const resultContainer = document.getElementById('resultContainer');
-    resultContainer.innerHTML = '<p>Loading...</p>';
+  const resultContainer = document.getElementById("resultContainer");
+  resultContainer.innerHTML = "<p>Loading...</p>";
 
-    const geocodeApiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(address)}`;
+  const geocodeApiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(
+    address
+  )}`;
 
-    fetch(geocodeApiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Geocoding API Error: ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(geoData => {
-            console.log('Geocoding Response:', geoData);
+  fetch(geocodeApiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Geocoding API Error: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((geoData) => {
+      console.log("Geocoding Response:", geoData);
 
-            if (geoData.length > 0) {
-                const { display_name, lat, lon } = geoData[0];
+      if (geoData.length > 0) {
+        const { display_name, lat, lon } = geoData[0];
 
-                fetchData(lat, lon, display_name);
-            } else {
-                resultContainer.innerHTML = `<p>Error: Unable to fetch coordinates for the provided address</p>`;
-            }
-        })
-        .catch(error => {
-            console.error('Geocoding API Error:', error);
+        fetchData(lat, lon, display_name);
+      } else {
+        resultContainer.innerHTML = `<p>Error: Unable to fetch coordinates for the provided address</p>`;
+      }
+    })
+    .catch((error) => {
+      console.error("Geocoding API Error:", error);
 
-            resultContainer.innerHTML = `<p>Error: Unable to fetch coordinates for the provided address</p>`;
-        });
+      resultContainer.innerHTML = `<p>Error: Unable to fetch coordinates for the provided address</p>`;
+    });
 }
 
 function fetchData(latitude, longitude, locationName) {
-    const apiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
+  const apiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
 
-    const resultContainer = document.getElementById('resultContainer');
-    resultContainer.innerHTML = '<p>Loading...</p>'; // Placeholder content
+  const resultContainer = document.getElementById("resultContainer");
+  resultContainer.innerHTML = "<p>Loading...</p>"; // Placeholder content
 
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Sunrise Sunset API Error: ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Sunrise Sunset API Response:', data);
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Sunrise Sunset API Error: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Sunrise Sunset API Response:", data);
 
-            const { sunrise, sunset, first_light, last_light, dawn, dusk, solar_noon, golden_hour, day_length, timezone } = data.results;
+      const {
+        sunrise,
+        sunset,
+        first_light,
+        last_light,
+        dawn,
+        dusk,
+        solar_noon,
+        golden_hour,
+        day_length,
+        timezone,
+      } = data.results;
 
-            const condensedLocationName = condenseLocationName(locationName);
+      const condensedLocationName = condenseLocationName(locationName);
 
-
-            resultContainer.innerHTML = `
+      resultContainer.innerHTML = `
                 <h3>${condensedLocationName}</h3>
                 <h4>Today:</h4>
                 <p>Sunrise: ${sunrise}</p>
@@ -81,22 +93,22 @@ function fetchData(latitude, longitude, locationName) {
                 <p>Timezone: ${timezone}</p>
                 <!-- Add more data as needed -->
             `;
-        })
-        .catch(error => {
-            console.error('Sunrise Sunset API Error:', error);
+    })
+    .catch((error) => {
+      console.error("Sunrise Sunset API Error:", error);
 
-            resultContainer.innerHTML = `<p>Error: Unable to fetch sunrise sunset data</p>`;
-        });
+      resultContainer.innerHTML = `<p>Error: Unable to fetch sunrise sunset data</p>`;
+    });
 }
 
 function condenseLocationName(fullLocationName) {
-    const parts = fullLocationName.split(', ');
+  const parts = fullLocationName.split(", ");
 
-    if (!isNaN(parts[1])) {
-        parts.splice(1, 1);
-    };
+  if (!isNaN(parts[1])) {
+    parts.splice(1, 1);
+  }
 
-    const condensedLocationName = parts.join(', ');
+  const condensedLocationName = parts.join(", ");
 
-    return condensedLocationName;
+  return condensedLocationName;
 }
